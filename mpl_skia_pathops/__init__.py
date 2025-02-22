59993bb28c18ba37a413b38dd40f33f236ee6904
Jae-Joon Lee <lee.j.joon@gmail.com>
2025-02-22 17:17:21 +0900

    expose union_all operation

--- mpl_skia_pathops/__init__.py
+++ mpl_skia_pathops/__init__.py
@@ -8,12 +8,13 @@
 # See https://github.com/pypa/setuptools/issues/1724#issuecomment-627241822
 from ._version import __version__
 
-__all__ = ["mpl2skia", "skia2mpl", "union", "intersection", "difference", "xor",
+__all__ = ["mpl2skia", "skia2mpl", "union", "union_all", "intersection", "difference", "xor",
            "stroke_to_fill",
            "PathOpsPathEffect"]
            # "PathOps", "PathOpsFromFunc", "PathOpsFromPatch", "PathOpsFromPath"]
 
-from .mpl_skia_pathops import (mpl2skia, skia2mpl, union, intersection, difference, xor,
+from .mpl_skia_pathops import (mpl2skia, skia2mpl, union, union_all,
+                               intersection, difference, xor,
                                stroke_to_fill)
 from .mpl_pe_pathops import (PathOpsPathEffect,)
                              # PathOps, PathOpsFromFunc, PathOpsFromPatch, PathOpsFromPath)
