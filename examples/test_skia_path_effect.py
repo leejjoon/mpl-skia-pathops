896de2c50fbbfa73dd26268bc70e657d39c66c69
Jae-Joon Lee <lee.j.joon@gmail.com>
2025-03-13 02:13:07 +0900

    updae test_skia_path_effect.py

--- examples/test_skia_path_effect.py
+++ examples/test_skia_path_effect.py
@@ -5,37 +5,97 @@ from mpl_skia_pathops.mpl_skia_pathops import stroke_with_patheffect
 
 import matplotlib.pyplot as plt
 from matplotlib.patches import PathPatch, Circle, Rectangle, Ellipse
+from matplotlib.patches import ArrowStyle
 
 
-fig, ax = plt.subplots(num=1, clear=True)
-ax.set_aspect(1)
+def prepare_axes(ax):
+    ax.set_aspect(1)
+    ax.set(xlim=(-200, 200), ylim=(-200, 200))
+    ax.patch.set_fc("gold")
 
-# p = TextPath((0, 0), "MA", size=200)
-p = Circle((0, 0), radius=100)
-skpath = mpl2skia(p)
 
-# p = Circle((0, 0), radius=5)
-# p = Rectangle((0, -2.5), 15, 5)
-p = Ellipse((0, 0), 30, 10)
+def get_patch(pe):
+    p = Circle((0, 0), radius=100)
+
+    skpath = mpl2skia(p)
+
+    skpath2 = stroke_with_patheffect(skpath, 1, pe, linejoin="round")
+    p2 = skia2mpl(skpath2)
+
+    pp2 = PathPatch(p2, fc="r", ec="r")
+
+    return pp2
+
+
+fig, axs = plt.subplots(2, 2, num=1, clear=True)
+
+# %%
+
+iax = iter(axs.flat)
+ax = next(iax)
+prepare_axes(ax)
+
+
+p = Circle((0, 0), radius=10)
 pepath = mpl2skia(p)
+advance = 30
 
-# pepath
-advance = 100
-
-pe = PathEffect.create_path1d(pepath, 45,
-                              # style=Path1DPathEffectStyle.MORPH,
-                              style=Path1DPathEffectStyle.ROTATE,
+pe = PathEffect.create_path1d(pepath, advance,
+                              style=Path1DPathEffectStyle.TRANSLATE,
                               )
-skpath2 = stroke_with_patheffect(skpath, 1, pe, linejoin="round")
-
-# p2 = skia2mpl(difference(skpath2, skpath))
-p2 = skia2mpl(skpath2)
-
-pp2 = PathPatch(p2, fc="w", ec="r")
+pp2 = get_patch(pe)
 ax.add_patch(pp2)
 
-ax.set(xlim=(-200, 200), ylim=(-200, 200))
-ax.patch.set_fc("gold")
+# %%
+
+ax = next(iax)
+prepare_axes(ax)
+
+
+p = Ellipse((0, 0), 30, 15)
+pepath = mpl2skia(p)
+advance = 42
+
+pe = PathEffect.create_path1d(pepath, advance,
+                              style=Path1DPathEffectStyle.ROTATE,
+                              )
+pp2 = get_patch(pe)
+ax.add_patch(pp2)
+
+# %%
+
+ax = next(iax)
+prepare_axes(ax)
+
+from matplotlib.patches import ConnectionStyle
+
+path = ConnectionStyle.Arc3().connect((0, 0), (60, 0))
+arrow, _ = ArrowStyle.Fancy().transmute(path, 40, 0)
+
+pepath = mpl2skia(arrow)
+advance = 79
+
+pe = PathEffect.create_path1d(pepath, advance,
+                              style=Path1DPathEffectStyle.MORPH,
+                              )
+pp2 = get_patch(pe)
+
+ax.add_patch(pp2)
+
+# %%
+
+ax = next(iax)
+prepare_axes(ax)
+
+
+p = Rectangle((0., -10), 2.5, 20)
+pepath = mpl2skia(p)
+advance = 2
+
+pe = PathEffect.create_path1d(pepath, advance,
+                              style=Path1DPathEffectStyle.TRANSLATE,
+                              )
+pp2 = get_patch(pe)
+ax.add_patch(pp2)
 
 plt.show()
-
